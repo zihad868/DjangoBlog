677f24f5087b36e725633af0e9c25127bf8600cf
zihad868 <zihadh868@gmail.com>
2024-01-31 20:08:51 +0600

    Render and view post

--- blog/blogApp/models.py
+++ blog/blogApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    title_tag = models.CharField(max_length=300)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    
+    def __str__(self) -> str:
+        return self.title +' | '+str(self.author)
--- blog/blogApp/urls.py
+++ blog/blogApp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import HomeView, ArticalDetail, AddPostView
 
 urlpatterns = [
-    path('', views.home, name='home')
+    # path('', views.home, name='home')
+    path('',HomeView.as_view(), name='home'),
+    path('artical-detail/<int:pk>', ArticalDetail.as_view(), name='artical-detail'),
+    path('add_post/', AddPostView.as_view(), name='add-post'),
 ]
