73668f6084fc09a38fe445a39640f310f8350221
zihad868 <zihadh868@gmail.com>
2024-02-01 11:44:15 +0600

    CRUD Section Complete

--- blog/blogApp/models.py
+++ blog/blogApp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length=300)
